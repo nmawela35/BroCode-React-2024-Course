23a274130846e8361ac055a1bd6637b2a6b3d5d4
Christopher Mawela <christopher.mawela@gmail.com>
2024-09-09 18:02:50 +0200

    ToDo list complete

--- 18todoList/src/ToDoList.jsx
+++ 18todoList/src/ToDoList.jsx
@@ -1,28 +1,58 @@
 import { useState } from "react";
 
 export default function ToDoList() {
-    const [tasks,setTasks] =useState([]);
+
     const [newTask,setNewTask] = useState("");
-
-    function inputChange(e){
-
-    }
+    const [tasks,setTasks] =useState([]);
 
     function addTask(){
-
+        if (newTask.trim() !== ''){
+            setTasks(tasks =>[...tasks,newTask])
+            setNewTask("")
+        }
     }
 
     function deleteTask(index){
-
+        const updatedTasks =tasks.filter((_,i) =>(i !== index)
+        )
+        setTasks(updatedTasks)
     }
 
     function moveTaskUp(index){
-
+        if (index>0){
+            const updatedTasks =[...tasks];
+            [updatedTasks[index],updatedTasks[index-1]]=[updatedTasks[index-1],updatedTasks[index]]
+            setTasks(updatedTasks)
+        }
     }
 
     function moveTaskDown(index){
+        if (index<tasks.length -1){
+            const updatedTasks =[...tasks];
+            [updatedTasks[index],updatedTasks[index+1]]=[updatedTasks[index+1],updatedTasks[index]]
+            setTasks(updatedTasks)
+        }
 
     }
 
-  return <></>;
+  return(
+    <div className="todo-list">
+        <h1>ToDo List</h1>
+        <div>
+            <input type="text" placeholder="Enter a task..." value={newTask} onChange={(e)=>setNewTask(e.target.value)}/>
+            <button className="add-button" onClick={addTask}>Add</button>
+        </div>
+        <ol>
+            {tasks.map((task,index)=>(
+                <li key={index}>
+                    <span className="text">{task}</span>
+                    <button className="delete-button" onClick={()=>deleteTask(index)}>Delete</button>
+                    <button className="move-button" onClick={()=>moveTaskUp(index)}>⬆️UP</button>
+                    <button className="move-button" onClick={()=>moveTaskDown(index)}>⬇️DOWN</button>
+                </li>
+            ))}
+        </ol>
+
+    </div>
+  )
 }
